9e653864d5dd1d2d3de5ea0a7ebb6fb20ab552bf
jlucasoliveira <lucasosilva68@gmail.com>
2021-07-22 10:26:31 -0300

    feat: Change home response and testing post request

--- lists/tests.py
+++ lists/tests.py
@@ -22,3 +22,8 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self) -> None:
         response = self.client.get("/")
         self.assertTemplateUsed(response, "lists/home.html")
+
+    def test_can_save_a_POST_request(self) -> None:
+        response = self.client.post("/", data={"item_text": "A new list item"})
+        self.assertIn("A new list item", response.content.decode("utf8"))
+        self.assertTemplateUsed(response, "lists/home.html")
--- lists/views.py
+++ lists/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 
 
 def home_page(request: HttpRequest) -> HttpResponse:
-    return render(request, "lists/home.html")
+    # if request.method == "POST":
+    #     return HttpResponse(request.POST["item_text"])
+    return render(request, "lists/home.html", {"new_item_text": request.POST.get("item_text", "")})
